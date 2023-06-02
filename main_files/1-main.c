c8c61422f3e17de4d7716927f6560e252127b608
masha-a-m <escritoramaree@gmail.com>
2023-06-02 14:28:12 -0700

    1 main

---
+++ main_files/1-main.c
@@ -0,0 +1,22 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include "binary_trees.h"
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+    binary_tree_print(root)
+    binary_tree_insert_left(root->right, 128);
+    binary_tree_insert_left(root, 54);
+    root->right = binary_tree_node(root, 402);
+    root = binary_tree_node(NULL, 98);
+    root->left = binary_tree_node(root, 12);
+     
+     printf("\n");
+     
+     return (0);
+}
